aa6e83a9745fffdd65d8b8be665cdeb77115e5f1
novelkhan <novel4004@gmail.com>
2024-08-04 03:11:08 +0600

    (//  Angular 16 project is created using 'npx -p @angular/cli@16 ng new portfolio' command. Guide Link- https://www.youtube.com/watch?v=LYNG3kcKRQ8 )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//  Angular 16 project is created using 'npx -p @angular/cli@16 ng new portfolio' command. Guide Link- https://www.youtube.com/watch?v=LYNG3kcKRQ8 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
