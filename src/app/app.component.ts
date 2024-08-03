ef6158d92b84ded6546f670770e0e3fc0fdd6829
novelkhan <novel4004@gmail.com>
2024-08-04 05:12:04 +0600

    Some comments are added in the app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 //  Angular 16 project is created using 'npx -p @angular/cli@16 ng new portfolio' command. Guide Link- https://www.youtube.com/watch?v=LYNG3kcKRQ8 
 
+
+
+//  Redirect problem solution is in this Link- https://www.youtube.com/watch?v=ZUEfQgPwDXI 
+//  _redirects File is added in src folder. And Its path is added in angular.json file 
+
+
+
+//  BootStrap-5 Installation Guide Link- https://www.youtube.com/watch?v=TIGy3VYsG5g  
+//  1. npm i bootstrap command is run to install bootstrap
+//  2. npm i @popperjs/core command is run to install bootstrap
+//  3. npm i bootstrap-icons command is run to install bootstrap 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
